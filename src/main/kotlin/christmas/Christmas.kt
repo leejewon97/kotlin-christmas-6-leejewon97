a464ea242b256022f78b502a242721b7d1d66245
Jtility <leejewon97@gmail.com>
2023-11-12 21:36:42 +0900

    feat(Christmas): 크리스마스 디데이(1~25) 할인가 입력
    함수 christmasDDayDiscount 생성

--- src/main/kotlin/christmas/Christmas.kt
+++ src/main/kotlin/christmas/Christmas.kt
@@ -1,5 +1,18 @@
 package christmas
 
+import christmas.enumeration.DDayDiscount
+import christmas.enumeration.December
+
 class Christmas {
     private val visitDate = InputView().readDate()
+    private val christmasDDayDiscount = christmasDDayDiscount(visitDate)
+
+    private fun christmasDDayDiscount(visitDate: Int): Int {
+        return when (visitDate) {
+            in December.FIRST.date..December.CHRISTMAS.date
+            -> DDayDiscount.START_DISCOUNT.value + visitDate * DDayDiscount.PER_DAY_DISCOUNT.value
+
+            else -> DDayDiscount.NO_DISCOUNT.value
+        }
+    }
 }
