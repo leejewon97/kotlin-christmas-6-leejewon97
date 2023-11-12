065750c9aefed5b9b5bf65794b5bd9aa1f995765
Jtility <leejewon97@gmail.com>
2023-11-12 22:22:24 +0900

    feat(December): 새로 쓰일 인스턴스들 생성
    WHOLE_WEEK, FIRST_WEEKEND, SECOND_WEEKEND 생성

--- src/main/kotlin/christmas/enumeration/December.kt
+++ src/main/kotlin/christmas/enumeration/December.kt
@@ -3,5 +3,8 @@ package christmas.enumeration
 enum class December(val date: Int) {
     FIRST(1),
     LAST(31),
-    CHRISTMAS(25);
+    CHRISTMAS(25),
+    WHOLE_WEEK(7),
+    FIRST_WEEKEND(1),
+    SECOND_WEEKEND(2);
 }
