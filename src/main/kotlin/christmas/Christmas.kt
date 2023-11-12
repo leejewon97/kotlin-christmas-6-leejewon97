8fc1a3170e5885989b3b8eeb83789932162037c9
Jtility <leejewon97@gmail.com>
2023-11-12 22:06:59 +0900

    refactor(christmasDDayDiscount): 빈 줄 삭제

--- src/main/kotlin/christmas/Christmas.kt
+++ src/main/kotlin/christmas/Christmas.kt
@@ -11,7 +11,6 @@ class Christmas {
         return when (visitDate) {
             in December.FIRST.date..December.CHRISTMAS.date
             -> DDayDiscount.START_DISCOUNT.value + visitDate * DDayDiscount.PER_DAY_DISCOUNT.value
-
             else -> DDayDiscount.NO_DISCOUNT.value
         }
     }
