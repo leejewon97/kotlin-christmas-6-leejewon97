662db8c91536e7566a8ae75277df90b974ae8a63
Jtility <leejewon97@gmail.com>
2023-11-12 18:27:21 +0900

    feat(InputView): 값 입력을 위한 class 생성
    fun readDate : 검증된 날짜를 return
    fun invalidDate : 잘못된 날짜를 재입력 받기 위한 검증
    return

---
+++ src/main/kotlin/christmas/InputView.kt
@@ -0,0 +1,24 @@
+package christmas
+
+import camp.nextstep.edu.missionutils.Console
+import christmas.enumeration.InputPhrase
+
+class InputView {
+    fun readDate(): Int {
+        println(InputPhrase.DATE.text)
+        val date = Console.readLine().toIntOrNull()
+        if (invalidDate(date)) {
+            try {
+                throw IllegalArgumentException(InputPhrase.ERROR_DATE.text)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                return readDate()
+            }
+        }
+        return date!!
+    }
+
+    private fun invalidDate(date: Int?): Boolean {
+        return date !in 1..31
+    }
+}
