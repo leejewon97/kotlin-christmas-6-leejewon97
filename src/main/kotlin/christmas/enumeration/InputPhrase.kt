d0b91ca40adba4a519b6843aa3c47ce327a904f0
Jtility <leejewon97@gmail.com>
2023-11-13 23:22:12 +0900

    feat(InputPhrase): 인스턴스 추가
    MENU, ERROR_MENU 추가

--- src/main/kotlin/christmas/enumeration/InputPhrase.kt
+++ src/main/kotlin/christmas/enumeration/InputPhrase.kt
@@ -2,5 +2,7 @@ package christmas.enumeration
 
 enum class InputPhrase(val text: String) {
     DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
-    ERROR_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    ERROR_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1"),
+    ERROR_MENU("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
 }
