0a5c1aae4243710431c99111a8cb75e08b954462
Jtility <leejewon97@gmail.com>
2023-11-13 23:05:29 +0900

    refactor(Christmas): 함수 반환을 표현식으로 변경

--- src/main/kotlin/christmas/Christmas.kt
+++ src/main/kotlin/christmas/Christmas.kt
@@ -8,15 +8,13 @@ class Christmas {
     private val christmasDDayDiscount = christmasDDayDiscount(visitDate)
     private val isWeekend = isWeekend(visitDate)
 
-    private fun christmasDDayDiscount(visitDate: Int): Int {
-        return when (visitDate) {
-            in December.FIRST.date..December.CHRISTMAS.date
-            -> DDayDiscount.START_DISCOUNT.value + visitDate * DDayDiscount.PER_DAY_DISCOUNT.value
-            else -> DDayDiscount.NO_DISCOUNT.value
-        }
+    private fun christmasDDayDiscount(visitDate: Int) = when (visitDate) {
+        in December.FIRST.date..December.CHRISTMAS.date ->
+            DDayDiscount.START_DISCOUNT.value + visitDate * DDayDiscount.PER_DAY_DISCOUNT.value
+
+        else -> DDayDiscount.NO_DISCOUNT.value
     }
 
-    private fun isWeekend(visitDate: Int): Boolean {
-        return visitDate % December.WHOLE_WEEK.date in December.FIRST_WEEKEND.date..December.SECOND_WEEKEND.date
-    }
+    private fun isWeekend(visitDate: Int) =
+        visitDate % December.WHOLE_WEEK.date in December.FIRST_WEEKEND.date..December.SECOND_WEEKEND.date
 }
