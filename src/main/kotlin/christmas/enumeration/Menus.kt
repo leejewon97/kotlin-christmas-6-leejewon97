771a9d106ae1b9886193b1b699043dec0b7b3259
Jtility <leejewon97@gmail.com>
2023-11-15 17:52:15 +0900

    feat(Menus): 새로운 MenuType 추가
    FOOD -> APPETIZER, MAIN, DESSERT

--- src/main/kotlin/christmas/enumeration/Menus.kt
+++ src/main/kotlin/christmas/enumeration/Menus.kt
@@ -1,21 +1,23 @@
 package christmas.enumeration
 
 enum class Menus(val menu: String, val price: Int, val type: MenuType) {
-    MUSHROOM_SOUP("양송이수프", 6_000, MenuType.FOOD),
-    TAPAS("타파스", 5_500, MenuType.FOOD),
-    CAESAR_SALAD("시저샐러드", 8_000, MenuType.FOOD),
-    T_BONE_STEAK("티본스테이크", 55_000, MenuType.FOOD),
-    BARBECUE_RIB("바비큐립", 54_000, MenuType.FOOD),
-    SEAFOOD_PASTA("해산물파스타", 35_000, MenuType.FOOD),
-    CHRISTMAS_PASTA("크리스마스파스타", 25_000, MenuType.FOOD),
-    CHOCOLATE_CAKE("초코케이크", 15_000, MenuType.FOOD),
-    ICE_CREAM("아이스크림", 5_000, MenuType.FOOD),
+    MUSHROOM_SOUP("양송이수프", 6_000, MenuType.APPETIZER),
+    TAPAS("타파스", 5_500, MenuType.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, MenuType.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55_000, MenuType.MAIN),
+    BARBECUE_RIB("바비큐립", 54_000, MenuType.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35_000, MenuType.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, MenuType.MAIN),
+    CHOCOLATE_CAKE("초코케이크", 15_000, MenuType.DESSERT),
+    ICE_CREAM("아이스크림", 5_000, MenuType.DESSERT),
     ZERO_COLA("제로콜라", 3_000, MenuType.DRINK),
     RED_WINE("레드와인", 60_000, MenuType.DRINK),
     CHAMPAGNE("샴페인", 25_000, MenuType.DRINK);
 
     enum class MenuType {
-        FOOD,
-        DRINK;
+        APPETIZER,
+        MAIN,
+        DESSERT,
+        DRINK
     }
 }
