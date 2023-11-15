5afe624e4e41bdfc820c3038cbdcad2d5a5b845e
Jtility <leejewon97@gmail.com>
2023-11-15 11:59:46 +0900

    feat(Order): 최소 최대 주문량을 표현하기 위한 열거형 클래스 생성

--- src/main/kotlin/christmas/enumeration/Order.kt
+++ src/main/kotlin/christmas/enumeration/Order.kt
@@ -3,4 +3,9 @@ package christmas.enumeration
 enum class Order(val index: Int) {
     MENU(0),
     QUANTITY(1);
+
+    enum class Quantity(val value: Int) {
+        MIN(1),
+        MAX(20);
+    }
 }
