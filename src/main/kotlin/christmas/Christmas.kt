efb4d96c56bebf974a364749791a35f1f10bdecc
Jtility <leejewon97@gmail.com>
2023-11-15 11:52:48 +0900

    refactor(Christmas): enum 클래스 Dicount의 변경사항 적용

--- src/main/kotlin/christmas/Christmas.kt
+++ src/main/kotlin/christmas/Christmas.kt
@@ -11,16 +11,16 @@ class Christmas {
 
     private fun christmasDDayDiscount(visitDate: Int) = when (visitDate) {
         in December.FIRST.date..December.CHRISTMAS.date ->
-            Discount.START_DISCOUNT.value + visitDate * Discount.PER_DAY_DISCOUNT.value
+            Discount.START_DISCOUNT.price + visitDate * Discount.PER_DAY_DISCOUNT.price
 
-        else -> Discount.NO_DISCOUNT.value
+        else -> Discount.NO_DISCOUNT.price
     }
 
     private fun isWeekend(visitDate: Int) =
         visitDate % December.WHOLE_WEEK.date in December.FIRST_WEEKEND.date..December.SECOND_WEEKEND.date
 
     private fun specialDiscount(visitDate: Int) = when {
-        visitDate == December.CHRISTMAS.date || visitDate % December.WHOLE_WEEK.date == December.FIRST_WEEKEND.date -> Discount.SPECIAL_DISCOUNT.value
-        else -> Discount.NO_DISCOUNT.value
+        visitDate == December.CHRISTMAS.date || visitDate % December.WHOLE_WEEK.date == December.FIRST_WEEKEND.date -> Discount.SPECIAL_DISCOUNT.price
+        else -> Discount.NO_DISCOUNT.price
     }
 }
