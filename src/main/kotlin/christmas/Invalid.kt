a891a54399b1b43d99ea7457b0ee3a2e9f80d910
Jtility <leejewon97@gmail.com>
2023-11-15 12:00:31 +0900

    refactor(Invalid): 열거형 클래스 Order 적용

--- src/main/kotlin/christmas/Invalid.kt
+++ src/main/kotlin/christmas/Invalid.kt
@@ -2,6 +2,7 @@ package christmas
 
 import christmas.enumeration.December
 import christmas.enumeration.Menus
+import christmas.enumeration.Order
 
 class Invalid {
     fun date(date: Int?): Boolean {
@@ -10,28 +11,34 @@ class Invalid {
 
     fun orders(ordersText: String): Boolean {
         val orders = ordersText.split(",")
-        return orders.any { invalidFormat(it) || invalidMenu(it.split("-")[0]) || invalidQuantity(it.split("-")[1]) } ||
+        return orders.any {
+            invalidFormat(it) || invalidMenu(it.split("-")[Order.MENU.index]) || invalidQuantity(
+                it.split(
+                    "-"
+                )[Order.QUANTITY.index]
+            )
+        } ||
                 overMaxQuantity(orders) || onlyDrink(orders) || notDistinctiveMenu(orders)
     }
 
     private fun notDistinctiveMenu(orders: List<String>): Boolean {
-        val menus = orders.map { it.split("-")[0] }
+        val menus = orders.map { it.split("-")[Order.MENU.index] }
         return menus.size != menus.distinct().size
     }
 
-    private fun onlyDrink(orders: List<String>) = orders.map { it.split("-")[0] }.all { menu ->
+    private fun onlyDrink(orders: List<String>) = orders.map { it.split("-")[Order.MENU.index] }.all { menu ->
         Menus.entries.filter { it.type == Menus.MenuType.DRINK }.map { it.menu }.contains(menu)
     }
 
     private fun overMaxQuantity(orders: List<String>) =
-        orders.sumOf { it.split("-")[1].toInt() } > 20
+        orders.sumOf { it.split("-")[Order.QUANTITY.index].toInt() } > Order.Quantity.MAX.value
 
     private fun invalidQuantity(quantity: String) =
-        quantity.toInt() < 1
+        quantity.toInt() < Order.Quantity.MIN.value
 
     private fun invalidMenu(menu: String) =
         !Menus.entries.map { it.menu }.contains(menu)
 
     private fun invalidFormat(order: String) =
-        order.split("-").size != 2 || order.split("-")[1].toIntOrNull() == null
+        order.split("-").size != 2 || order.split("-")[Order.QUANTITY.index].toIntOrNull() == null
 }
