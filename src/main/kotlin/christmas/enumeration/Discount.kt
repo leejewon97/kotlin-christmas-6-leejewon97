62152f61a4dcf63fdd68e3c1e386e640e972f72c
Jtility <leejewon97@gmail.com>
2023-11-15 11:51:25 +0900

    refactor(Discount): 매개변수명 변경 value -> price

--- src/main/kotlin/christmas/enumeration/Discount.kt
+++ src/main/kotlin/christmas/enumeration/Discount.kt
@@ -1,6 +1,6 @@
 package christmas.enumeration
 
-enum class Discount(val value: Int) {
+enum class Discount(val price: Int) {
     START_DISCOUNT(1000),
     PER_DAY_DISCOUNT(100),
     NO_DISCOUNT(0),
