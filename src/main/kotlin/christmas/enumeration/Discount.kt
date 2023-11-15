608b0765fabdb53f853dfd82954cf7bfe6c22ccb
Jtility <leejewon97@gmail.com>
2023-11-15 23:05:33 +0900

    fix(Discount): START_DISCOUNT, SPECIAL_DISCOUNT의 price에 "_"추가

--- src/main/kotlin/christmas/enumeration/Discount.kt
+++ src/main/kotlin/christmas/enumeration/Discount.kt
@@ -1,10 +1,10 @@
 package christmas.enumeration
 
 enum class Discount(val price: Int) {
-    START_DISCOUNT(1000),
+    START_DISCOUNT(1_000),
     PER_DAY_DISCOUNT(100),
     NO_DISCOUNT(0),
-    SPECIAL_DISCOUNT(1000),
+    SPECIAL_DISCOUNT(1_000),
     ENABLE_DISCOUNT_STANDARD(10_000),
     PRESENT_STANDARD(120_000),
     PRESENT_DISCOUNT(25_000),
