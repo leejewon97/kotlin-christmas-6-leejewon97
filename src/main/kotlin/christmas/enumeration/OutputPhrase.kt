ed7fe7a78bd082cc29177c0556b5d7fd388b62d2
Jtility <leejewon97@gmail.com>
2023-11-15 21:24:41 +0900

    feat(OutputPhrase): 내부 클래스 Discount 생성
    혜택 내역 출력 시 필요한 값 입력

--- src/main/kotlin/christmas/enumeration/OutputPhrase.kt
+++ src/main/kotlin/christmas/enumeration/OutputPhrase.kt
@@ -20,4 +20,12 @@ enum class OutputPhrase(val text: String) {
         TEN_THOUSAND("트리"),
         TWENTY_THOUSAND("산타");
     }
+
+    enum class Discount(val text: String) {
+        CHRISTMAS_D_DAY("크리스마스 디데이 할인: -"),
+        WEEKDAY("평일 할인: -"),
+        WEEKEND("주말 할인: -"),
+        SPECIAL("특별 할인: -"),
+        PRESENT("증정 이벤트: -");
+    }
 }
