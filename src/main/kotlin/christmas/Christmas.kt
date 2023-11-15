9c378cfc697f6ab4c310595fe6ca7debd1a0922a
Jtility <leejewon97@gmail.com>
2023-11-15 16:53:08 +0900

    feat(Christmas): 총주문 금액 계산 추가
    orders를 전부 돌며 Menus.valueOf(menu) 해당하는 Menus.price * quantity의 합 반환
    총주문 금액이 10,000원이 안 되면 이벤트 적용을 안 하기 때문에 모든 할인가 0 할당
    value 할인가들을 variable로 변경

--- src/main/kotlin/christmas/Christmas.kt
+++ src/main/kotlin/christmas/Christmas.kt
@@ -2,13 +2,23 @@ package christmas
 
 import christmas.enumeration.December
 import christmas.enumeration.Discount
+import christmas.enumeration.Menus
 
 class Christmas {
     private val visitDate = InputView().readDate()
-    private val christmasDDayDiscount = christmasDDayDiscount(visitDate)
+    private var christmasDDayDiscount = christmasDDayDiscount(visitDate)
     private val isWeekend = isWeekend(visitDate)
-    private val specialDiscount = specialDiscount(visitDate)
+    private var dayOfWeekDiscount = 0
+    private var specialDiscount = specialDiscount(visitDate)
     private val orders = InputView().readOrdersText()
+    private val totalPrice = calculateTotalPrice()
+
+    init {
+        if (totalPrice < Discount.ENABLE_DISCOUNT_STANDARD.price) {
+            christmasDDayDiscount = Discount.NO_DISCOUNT.price
+            specialDiscount = Discount.NO_DISCOUNT.price
+        }
+    }
 
     private fun christmasDDayDiscount(visitDate: Int) = when (visitDate) {
         in December.FIRST.date..December.CHRISTMAS.date ->
@@ -23,5 +33,13 @@ class Christmas {
     private fun specialDiscount(visitDate: Int) = when {
         visitDate == December.CHRISTMAS.date || visitDate % December.WHOLE_WEEK.date == December.FIRST_WEEKEND.date -> Discount.SPECIAL_DISCOUNT.price
         else -> Discount.NO_DISCOUNT.price
+    }
+
+    private fun calculateTotalPrice(): Int {
+        var totalPrice = 0
+        orders.forEach { (menu, quantity) ->
+            totalPrice += Menus.valueOf(menu).price * quantity
+        }
+        return totalPrice
     }
 }
